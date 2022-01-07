fab3b848232e843516fd4d8be3f58935115dee0d
dtwawa <69995116+dtwawa@users.noreply.github.com>
2022-01-07 14:53:26 +0800

    Update gua_opencard93.js

--- gua_opencard93.js
+++ gua_opencard93.js
@@ -37,7 +37,7 @@ cron:30 1 6-31/2,31 1 *
 30 1 6-31/2,31 1 * https://raw.githubusercontent.com/smiek2121/scripts/master/gua_opencard93.js, tag=1.6～1.31 会员狂欢日, enabled=true
 
 */
-let guaopencard_addSku = "false"
+let guaopencard_addSku = "true"
 let guaopencard = "true"
 let guaopenwait = "0"
 let guaopencard_draw = "3"
