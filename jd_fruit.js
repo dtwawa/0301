a51e9ee769cdda8b61b7add4d7d5d9e35e725054
dtwawa <69995116+dtwawa@users.noreply.github.com>
2023-04-26 11:19:53 +0800

    Update jd_fruit.js

--- jd_fruit.js
+++ jd_fruit.js
@@ -28,7 +28,7 @@ let cookiesArr = [], cookie = '', jdFruitShareArr = [], isBox = false, notify, n
 //助力好友分享码(最多3个,否则后面的助力失败),原因:京东农场每人每天只有3次助力机会
 //此此内容是IOS用户下载脚本到本地使用，填写互助码的地方，同一京东账号的好友互助码请使用@符号隔开。
 //下面给出两个账号的填写示例（iOS只支持2个京东账号）
-let shareCodes = [ 'd8fad18dc0454b01be13166323b57fff@5888f5d7a15f409db04c2f8acd32169b@91b1f191b4004ffca608a37d15e945ba']
+let shareCodes = [ 'd8fad18dc0454b01be13166323b57fff@5888f5d7a15f409db04c2f8acd32169b@91b1f191b4004ffca608a37d15e945ba@9cc9fcd1439b467284992bd91f84093c@4709b4eb683a44b79a06b3188a6c4241']
 let message = '', subTitle = '', option = {}, isFruitFinished = false;
 const retainWater = 100;//保留水滴大于多少g,默认100g;
 let jdNotify = false;//是否关闭通知，false打开通知推送，true关闭通知推送
